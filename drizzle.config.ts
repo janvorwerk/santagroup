476f29ea43b3a7a78ccc04dbedb1a6c988db6603
Jan Vorwerk <139635115+janvorwerk@users.noreply.github.com>
2026-01-04 19:17:59 +0100

    Model

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'drizzle-kit';
+import * as dotenv from "dotenv";
+
+import { defineConfig } from "drizzle-kit";
+
+// Read local connection file
+dotenv.config({ path: ".env.local" });
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set');
+  throw new Error("DATABASE_URL environment variable is not set");
 }
 
 export default defineConfig({
-  schema: './lib/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  breakpoints: false,
   dbCredentials: {
     url: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
-
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,15 +1,38 @@
 import { sql } from "drizzle-orm";
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { serial, pgTable, text, timestamp, uuid, foreignKey } from "drizzle-orm/pg-core";
 
 export const pool = pgTable("pool", {
   id: uuid("id")
     .primaryKey()
     .notNull()
     .default(sql`gen_random_uuid()`),
-  key: uuid("key")
-    .notNull()
-    .default(sql`gen_random_uuid()`),
   name: text("name").notNull(),
-  admin: text("admin").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const group = pgTable("group", {
+  id: serial("id").primaryKey(),
+  poolId: uuid("pool_id").references(() => pool.id, { onDelete: "cascade" }),
+});
+
+export const player = pgTable(
+  "player",
+  {
+    id: uuid("id")
+      .primaryKey()
+      .notNull()
+      .default(sql`gen_random_uuid()`),
+    groupId: serial("group_id").references(() => group.id, { onDelete: "cascade" }),
+    name: text("name").notNull(),
+    toId: uuid("to_id"),
+  },
+
+  (table) => {
+    return {
+      parentReference: foreignKey({
+        columns: [table.toId],
+        foreignColumns: [table.id],
+      }),
+    };
+  }
+);
